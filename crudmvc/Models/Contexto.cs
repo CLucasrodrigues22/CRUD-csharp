b2a13e614b0955bac422277f39fa1e101e4dd6bf
CLucasrodrigues22 <c.lucasrodrigues22@gmail.com>
2023-03-06 22:00:52 -0300

    Firts commit

---
+++ crudmvc/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crudmvc.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto( DbContextOptions<Contexto> options ) : base(options)
+        { 
+            
+        }
+
+        public DbSet<Profissional> Profissional { get; set; }
+    }
+}
---
+++ crudmvc/Models/Profissional.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace crudmvc.Models
+{
+    [Table("Profissionais")]
+    public class Profissional
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string? Nome { get; set; }
+
+        [Column("Telefone")]
+        [Display(Name = "Telefone")]
+        public string? Telefone { get; set; }
+
+        [Column("Rg")]
+        [Display(Name = "RG")]
+        public string? Rg { get; set; }
+
+        [Column("Endereco")]
+        [Display(Name = "Endereço")]
+        public string? Endereco { get; set; }
+
+        [Column("Salario")]
+        [Display(Name = "Salário")]
+        public string? Salario { get; set; }
+    }
+}
